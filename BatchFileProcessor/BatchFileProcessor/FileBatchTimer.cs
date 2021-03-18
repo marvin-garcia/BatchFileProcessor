60ea47c112b02d113d55cf1952045c04701ac6b9
Marvin Garcia <magar@microsoft.com>
2021-03-17 21:58:22 -0400

    updates to functions

---
+++ BatchFileProcessor/BatchFileProcessor/FileBatchTimer.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace BatchFileProcessor
+{
+    public static class FileBatchTimer
+    {
+        [FunctionName("FileBatchTimer")]
+        public static async Task Run(
+            [TimerTrigger("*/30 * * * * *")] TimerInfo myTimer,
+            [DurableClient] IDurableEntityClient entityClient,
+            [DurableClient] IDurableOrchestrationClient orchestrationClient,
+            [CosmosDB(
+                databaseName: "%CosmosDatabaseName%",
+                collectionName: "%CosmosCollectionName%",
+                ConnectionStringSetting = "CosmosDBConnection")]IAsyncCollector<FileBatch> cosmosDBOut,
+            ILogger log)
+        {
+            try
+            {
+                // Get entity state
+                string entityName = "BatchEntity";
+                var entityId = new EntityId("FileBatch", entityName);
+                var entityState = await entityClient.ReadEntityStateAsync<FileBatch>(entityId);
+
+                // Check if entity exists and is outdated
+                if (entityState.EntityExists)
+                {
+                    if (entityState.EntityState.IsActive && entityState.EntityState.IsPastDue)
+                    {
+                        log.LogInformation($"FileBatchTimer will reset entity because it is outdated");
+
+                        // Write batch to DB
+                        await cosmosDBOut.AddAsync(entityState.EntityState);
+                        
+                        // Signal entity deletion
+                        await entityClient.SignalEntityAsync(entityId, "Delete");
+                        await entityClient.CleanEntityStorageAsync(true, true, new CancellationToken());
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                log.LogError($"FileBatchTimer failed with exception {e}");
+            }
+        }
+    }
+}
